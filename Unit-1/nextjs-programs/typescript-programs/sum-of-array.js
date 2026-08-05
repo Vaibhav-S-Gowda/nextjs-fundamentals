aa380303ea5ce1c6001f87d0f9686e7c7f68ad15
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-08-05 17:19:36 +0530

    feat(ts): add sum of array with typed return values

---
+++ Unit-1/nextjs-programs/typescript-programs/sum-of-array.js
@@ -0,0 +1,7 @@
+"use strict";
+let numbers = [10, 20, 30, 40, 50];
+let total = 0;
+for (let num of numbers) {
+    total += num;
+}
+console.log(total);
---
+++ Unit-1/nextjs-programs/typescript-programs/sum-of-array.ts
@@ -0,0 +1,8 @@
+let numbers: number[] = [10, 20, 30, 40, 50];
+let total = 0;
+
+for (let num of numbers) {
+    total += num;
+}
+
+console.log(total);
