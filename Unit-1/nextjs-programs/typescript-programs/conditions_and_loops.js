b474b5ff90b6b93ac98965a91cfdd87e41f67d41
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-08-05 17:19:20 +0530

    feat(ts): add conditionals and loops examples

---
+++ Unit-1/nextjs-programs/typescript-programs/conditions_and_loops.js
@@ -0,0 +1,12 @@
+"use strict";
+let x = 50;
+let y = 30;
+if (x > y) {
+    console.log(x + " is larger");
+}
+else {
+    console.log(y + " is larger");
+}
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
---
+++ Unit-1/nextjs-programs/typescript-programs/conditions_and_loops.ts
@@ -0,0 +1,12 @@
+let x: number = 50;
+let y: number = 30;
+
+if (x > y) {
+    console.log(x + " is larger");
+} else {
+    console.log(y + " is larger");
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
