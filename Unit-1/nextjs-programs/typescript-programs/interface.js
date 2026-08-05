821b6b103854968eccf224c746140ddca942d0f0
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-08-05 17:19:29 +0530

    feat(ts): add interface and object typing examples

---
+++ Unit-1/nextjs-programs/typescript-programs/interface.js
@@ -0,0 +1,6 @@
+"use strict";
+let student = {
+    name: "Shetty",
+    age: 22
+};
+console.log(student);
---
+++ Unit-1/nextjs-programs/typescript-programs/interface.ts
@@ -0,0 +1,11 @@
+interface Student {
+    name: string;
+    age: number;
+}
+
+let student: Student = {
+    name: "Shetty",
+    age: 22
+}
+
+console.log(student);
