37ade1ff40a8e52ab2c09df05de649ab9f3fd491
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-08-05 17:19:13 +0530

    feat(ts): add data types demonstration

---
+++ Unit-1/nextjs-programs/typescript-programs/data_types.js
@@ -0,0 +1,29 @@
+"use strict";
+let name1 = "Shetty";
+let age = 22;
+let isStudent = true;
+console.log(name1);
+console.log(age);
+console.log(isStudent);
+let num1 = 35;
+let num2 = 45;
+let sum = num1 + num2;
+console.log("Sum = ", sum);
+console.log("Subtract = ", num1 - num2);
+console.log("Multiply = ", num1 * num2);
+console.log("Divide = ", num1 / num2);
+console.log("Remainder = ", num1 % num2);
+console.log("Square of = ", num1 ** num2);
+let integer = 10;
+let decimal = 10.56;
+let negative = -40;
+let exponential = 2e5;
+console.log(integer, " ", decimal, " ", negative, " ", exponential);
+// ----- String ----- 
+let firstName = "Shashank";
+let lastName = "Shetty";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+// Template Literals
+console.log(`My name is ${fullName}`);
+console.log(`I am ${age} years old.`);
---
+++ Unit-1/nextjs-programs/typescript-programs/data_types.ts
@@ -0,0 +1,32 @@
+let name1: string = "Shetty";
+let age: number = 22;
+let isStudent: boolean = true;
+console.log(name1);
+console.log(age);
+console.log(isStudent);
+
+let num1: number = 35;
+let num2: number = 45;
+let sum = num1 + num2;
+console.log("Sum = ", sum);
+console.log("Subtract = ", num1 - num2);
+console.log("Multiply = ", num1 * num2);
+console.log("Divide = ", num1 / num2);
+console.log("Remainder = ", num1 % num2);
+console.log("Square of = ", num1 ** num2);
+
+let integer: number = 10;
+let decimal: number = 10.56;
+let negative: number = -40;
+let exponential: number = 2e5;
+console.log(integer, " ", decimal, " ", negative, " ", exponential);
+
+// ----- String ----- 
+let firstName: string = "Shashank";
+let lastName: string = "Shetty";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Template Literals
+console.log(`My name is ${fullName}`);
+console.log(`I am ${age} years old.`)
