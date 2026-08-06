33f5a3ddd3b48bbd05e9dd10364e0244eb01dbc8
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-08-06 18:05:10 +0530

    Add introduction exercise

---
+++ Unit-1/01-introduction/z-exercise.ts
@@ -0,0 +1,44 @@
+let accountHolder :string = "Kaushalya";
+let accountNumber: symbol = Symbol(7612529840);
+let balance: number = 87980.70985;
+let isActive: boolean = true;
+console.log("----- Bank Account Details -----");
+console.log("Account Holder    :", accountHolder);
+console.log("Account Number    :", accountNumber);
+console.log("Balance           :", balance.toFixed(2));
+console.log("Is Account active :", isActive);
+
+
+let brand: string = "Aston Martin";
+let model: string = "Vanquish";
+let price: number = 88576900.503;
+let isElectric: boolean = false;
+console.log("----- Car Information -----");
+console.log("Brand      :", brand);
+console.log("Model      :", model);
+console.log("Price      :", price.toFixed(2));
+console.log("IsElectric :", isElectric);
+
+
+let movieName: string = "TOXIC";
+let rating: number = 8.7865;
+let releaseYear: number = 2026;
+let isAvailable: boolean = false;
+console.log("----- Movie Information -----");
+console.log("Movie Name          :", movieName);
+console.log("Rating              :", rating.toFixed(1));
+console.log("Release Year        :", releaseYear);
+console.log("Is Available on OTT :", isAvailable );
+
+
+let title: string = "The Great Gatsby";
+let author: string = "F. Scott Fitzgerald";
+let pages: number = 180;
+let bookPrice: number = 1900.509875;
+let inStock: boolean = true;
+console.log("----- Book Details -----");
+console.log("Book Title       :", title);
+console.log("Author           :", author);
+console.log("Pages            :", pages);
+console.log("Price            :", bookPrice.toFixed(2));
+console.log("Is book in stock :", inStock);
