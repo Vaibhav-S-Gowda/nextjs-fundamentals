42225ceaa441693fef8be948f8410859c80dafdb
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-08-06 18:05:09 +0530

    Add student information example

---
+++ Unit-1/01-introduction/student-information.ts
@@ -0,0 +1,9 @@
+let studentName: string = "Hinduja";
+let age: number = 22;
+let cgpa: number = 8.75;
+let isPlaced: boolean = true;
+
+console.log("Student Name :", studentName);
+console.log("Age          :", age);
+console.log("CGPA         :", cgpa);
+console.log("Placed       :", isPlaced);
