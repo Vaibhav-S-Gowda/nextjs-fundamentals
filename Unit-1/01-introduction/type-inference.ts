f94c913abdd87cc0bc2aaa5a312ab8c8953e4535
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-08-06 18:05:09 +0530

    Add type inference introduction

---
+++ Unit-1/01-introduction/type-inference.ts
@@ -0,0 +1,7 @@
+let city = "Bangalore";
+let temperature = 30;
+let isRaining = false;
+
+console.log(city, " - ", typeof (city));
+console.log(temperature, " - ", typeof (temperature));
+console.log(isRaining, " - ", typeof (isRaining));
