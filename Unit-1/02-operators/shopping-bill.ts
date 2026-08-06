cdef0e6050b256c50a167f43a8b83603925392bb
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-08-06 18:05:10 +0530

    Add shopping bill operator example

---
+++ Unit-1/02-operators/shopping-bill.ts
@@ -0,0 +1,12 @@
+let laptopPrice = 65000;
+let quantity = 2;
+
+let subtotal = laptopPrice * quantity;
+
+let deliveryCharge = 500;
+
+let totalBill = subtotal + deliveryCharge;
+
+console.log("Subtotal :", subtotal);
+console.log("Delivery :", deliveryCharge);
+console.log("Total    :", totalBill);
